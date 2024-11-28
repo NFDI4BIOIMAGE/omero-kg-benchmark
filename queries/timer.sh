f39ae4692f3e49d0a5aa072d73df8b9a6bd1004d
Carsten Fortmann-Grote <grotec@evolbio.mpg.de>
2024-11-28 17:31:38 +0100

    Fixing timer script.

--- queries/timer.sh
+++ queries/timer.sh
@@ -1,7 +1,8 @@
 #! /bin/bash
 
-if test "$#" -neq 2 ]]; then
-    echo("Usage: timer.sh QUERYFILE ENDPOINTURL")
+if test "$#" -ne 2; then
+    echo "Usage: timer.sh QUERYFILE ENDPOINTURL"
+    exit 1
 fi
 
 QUERY=$1
@@ -18,8 +19,8 @@ echo "Wall (s),User (s),Sys(s)" > $RESULTSFNAME
 
 # Run timer NQUERIES times
 for (( i=1; i<NQUERIES; i++ )); {
-    $TIME -f "%e,%U,%S" -a -o $RESULTSFNAME $SPARQL --service $ENDPOINT --query $QUERY
+    $TIME -f "%e,%U,%S" -a -o $RESULTSFNAME $SPARQL --service $ENDPOINT --query $QUERY > /dev/null 2>&1
 }
 
-Write out mean and std.
+# Write out mean and std.
 python -c "import pandas; print(pandas.read_csv(\"${RESULTSFNAME}\").describe().loc[['mean', 'std']])"
